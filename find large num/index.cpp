aeca1a76aa9aeb58a9b5588daa3d4a4e9266bd13
Fazal-Code <fazal.rehman@techsembly.com>
2024-01-09 17:44:43 +0500

    today make two program on cpp

---
+++ find large num/index.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 10;
+    int b = 12;
+    int c = 8;
+
+    if (a > b)
+    {
+        cout << "A Is Bigger Num";
+    }
+    else if (b > a)
+    {
+        cout << "B Is Bigger Num";
+    }
+    else if (c > a)
+    {
+        cout << "B Is Bigger Num";
+    }
+    else
+    {
+        cout << "No Answer";
+    }
+
+    return 0;
+}
---
+++ intrest calculater/index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int p = 100;
+    int r = 5;
+    int t = 2;
+
+    int SI;
+    SI = p*r*t / 100;
+    cout << SI;
+
+    return 0;
+}
