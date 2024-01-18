621154550b48376b512efc3f73fe70e4e389af1f
Fazal-Code <fazal.rehman@techsembly.com>
2024-01-18 08:50:27 +0500

    update code WFH , its my Day

--- OOP-Udemy/section 5/while-loop/index.cpp
+++ OOP-Udemy/section 5/while-loop/index.cpp
@@ -6,20 +6,9 @@ int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 
-	int num;
-	// fill the user any number 
-	cin >> num;
+	
 
-	int i = num;
-	while(i > 0){
-		if (i > num){
-		  cout << i << "true" <<endl;
-		  
-		}else {
-			break;
-		}
-		i++;
-	}
+
 
 	return 0;
 }
